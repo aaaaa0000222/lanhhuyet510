796389fe77ba3a44369bd6ea16686de84b754e2e
thjkjnoidi <33460449+thjkjnoidi@users.noreply.github.com>
2017-12-27 00:29:15 +0800

    Update DICE_rand_para_installer.js

--- DICE_rand_para_installer.js
+++ DICE_rand_para_installer.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         LuckyDICEBOT
-// @namespace    https://www.facebook.com/
+// @namespace    https://www.facebook.com/lanhhuyet510
 // @version      2.0
 // @description  3333
-// @author       cc
+// @author       Ngo Anh Duc
 // @match        https://luckygames.io/*
 // @require      https://raw.githubusercontent.com/thjkjnoidi/lanhhuyet510/master/code.js
 // @updateURL    https://github.com/thjkjnoidi/lanhhuyet510/blob/master/DICE_rand_para_installer.js
