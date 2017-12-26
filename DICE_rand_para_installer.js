3114533d5d9e6b94c56f430aacb0f110a3dee2d5
thjkjnoidi <33460449+thjkjnoidi@users.noreply.github.com>
2017-12-26 23:53:19 +0800

    Create DICE_rand_para_installer.js

---
+++ DICE_rand_para_installer.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         LuckyDICEBOT
+// @namespace    https://www.facebook.com/
+// @version      2.0
+// @description  3333
+// @author       cc
+// @match        https://luckygames.io/*
+// @require      https://raw.githubusercontent.com/thjkjnoidi/lanhhuyet510/master/code.js
+// @updateURL    https://github.com/thjkjnoidi/lanhhuyet510/LuckyGames/blob/master/DICE_rand_para_installer.js
+// @grant        none
+// ==/UserScript==
